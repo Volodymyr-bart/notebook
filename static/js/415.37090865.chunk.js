16f1882d952f233f72cdc703f9cf98de892d4461
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-02-28 11:34:36 +0000

    Deploying to gh-pages from @ Volodymyr-bart/notebook@30600586b019d2390d6c7c48dcb8ef6a1961d6d9 🚀

---
+++ static/js/415.37090865.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknotebook=self.webpackChunknotebook||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var i=n(184),o={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,i.jsx)("div",{style:o.container,children:(0,i.jsx)("h1",{style:o.title,children:"Welcome to Notepad"})})}}}]);
+//# sourceMappingURL=415.37090865.chunk.js.map
